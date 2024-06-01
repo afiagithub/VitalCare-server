8127e06aee025a9c106012806d5c91d7210c8dd7
Afia Rahman <rahmanmou26@gmail.com>
2024-06-01 18:16:11 +0600

    created api to add users to database

--- index.js
+++ index.js
@@ -27,6 +27,20 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
+        const userCollection = client.db('diagnosDB').collection('users')
+
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const isExist = await userCollection.findOne(query);
+            if (isExist) {
+                return res.send({ message: 'User Already Exists' })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result)
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
